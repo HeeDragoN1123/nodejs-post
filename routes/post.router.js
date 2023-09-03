b6cc5955c2a6973fc5d96b3fe60d199e44c44ccf
HeeDragoN1123 <yhy7952@gmail.com>
2023-09-03 18:26:24 +0900

    post 등록 조회 상세조회 수정 삭제 완료

--- routes/post.router.js
+++ routes/post.router.js
@@ -30,116 +30,123 @@ router.post('/posts', async (req, res, next) => {
         .json({ message: '데이터 형식이 올바르지 않습니다.' });
     }
   });
-
-
-/* 게시판 목록 조회 */
-router.get('/posts',  async(req, res, next) => {
-
-    const cheakpost = await Schemapost.find().sort({createdAt : -1}).exec();
-
-    const mapCheakpost = cheakpost.map(post =>({
-        postId :post._id,  // _id 를 postId로 변경 _id 
-        user: post.user,
-        title : post.title,
-        createdAt: post.createdAt
-    }));
-
-
-    return res.status(200).json({ mapCheakpost });
-
+  
+  /* 게시판 목록 조회 */
+  router.get('/posts', async (req, res, next) => {
+    const cheakpost = await prisma.posts.findMany({
+      select: {
+        postId: true,
+        user: true,
+        title: true,
+        createdAt: true,
+      },
+      orderBy: {
+        createdAt: 'desc', // createdAt을 내림차순으로 정렬
+      },
+    });
+  
+    return res.status(200).json({ data: cheakpost });
   });
-
-
-/* 게시글 상세 조회 */
-router.get('/posts/:_postId', async(req, res, next) => {
-try{
-    const {_postId} = req.params;
-
-    const cheak1post = await Schemapost.findById(_postId).exec();
-    
-    const mapCheak1post = {
-        postId :cheak1post._id,
-        user: cheak1post.user,
-        content : cheak1post.content,
-        createdAt: cheak1post.createdAt
+  
+  /* 게시글 상세 조회 */
+  router.get('/posts/:postId', async (req, res, next) => {
+    try {
+      const { postId } = req.params;
+  
+      //const cheak1post = await Schemapost.findById(_postId).exec();
+      const cheak1post = await prisma.posts.findFirst({
+        where: { postId: +postId },
+        select: {
+          postId: true,
+          user: true,
+          content: true,
+          createdAt: true,
+        },
+      });
+  
+      return res.status(201).json({ data: cheak1post });
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(400)
+        .json({ message: '데이터 형식이 올바르지 않습니다.' });
     }
-
-    return res.status(201).json({mapCheak1post});
-
-} catch(err){
-    console.error(err);
-    return res.status(400).json({message : '데이터 형식이 올바르지 않습니다.'})
-}
-
-});
-
-/* 게시글 수정 */
-router.put('/posts/:_postId', async(req, res, next) => {
-    try{
-        const {_postId} = req.params;
-        const {password , title, content} = req.body;
-    
-    
-        const editpost = await Schemapost.findById(_postId).exec();
-    
-        if(!editpost) {
-            return res.status(404).json({message : '게시글 조회에 실패하였습니다.'})
-        }
-    
-        if(editpost.password !== password){
-            return res.status(400).json({message : '비밀번호가 맞지 않습니다.'})
-        }
-    
-        editpost.title  = title;
-    
-        editpost.content = content;
-    
-    
-        await editpost.save();
-        
-    
-        return res.status(200).json({message: '게시글이 수정되었습니다.'})
-    } catch(err){    
-        console.error(err);
-        return res.status(400).json({message : '데이터 형식이 올바르지 않습니다.'})
-
-            
-
+  });
+  
+  /* 게시글 수정 */
+  router.put('/posts/:postId', async (req, res, next) => {
+    try {
+      const { postId } = req.params;
+      const { password, title, content } = req.body;
+  
+      const editpost = await prisma.posts.findUnique({
+        where: { postId: +postId },
+      });
+  
+      if (!editpost) {
+        return res.status(404).json({ message: '게시글 조회에 실패하였습니다.' });
+      }
+  
+      if (editpost.password !== password) {
+        return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+      }
+  
+      await prisma.posts.update({
+        data: { title, content },
+        where: {
+          postId: +postId,
+          password,
+        },
+      });
+  
+      return res.status(200).json({ message: '게시글이 수정되었습니다.' });
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(400)
+        .json({ message: '데이터 형식이 올바르지 않습니다.' });
     }
-
-});
-
-/* 게시글 삭제 */
-router.delete('/posts/:_postId', async(req,res,next)=>{
-    try{
-        const {_postId} = req.params;
-        const {password} = req.body;
-    
-        const  deletepost = await Schemapost.findById(_postId).exec();
-
-        if(!deletepost){
-            return res.status(404).json({message : '게시글 조회에 실패하였습니다.'})
-        }
-
-    
-        await deletepost.deleteOne({password: _postId});
-        
-        if(deletepost.password !== password){
-            return res.status(400).json({message : '비밀번호가 맞지 않습니다.'})
-        }
-    
-        return res.status(200).json({message : '게시글을 삭제하였습니다.'});
-    
-    } catch(err){
-        console.error(err);
-        return res.status(400).json({message : '데이터 형식이 올바르지 않습니다.'})
-        
+  });
+  
+  /* 게시글 삭제 */
+  router.delete('/posts/:postId', async (req, res, next) => {
+    try {
+      const { postId } = req.params;
+      const { password } = req.body;
+  
+      /* postId 찾기 */
+      const deletepost = await prisma.posts.findUnique({
+        where: {
+          postId: +postId,
+          password,
+        },
+      });
+  
+      /* 조건 */
+      if (!deletepost) {
+        return res.status(404).json({ message: '게시글 조회에 실패하였습니다.' });
+      }
+  
+      if (deletepost.password !== password) {
+        return res.status(400).json({ message: '비밀번호가 맞지 않습니다.' });
+      }
+  
+      /* 조건을 통과하면 게시글 삭제 */
+      await prisma.posts.delete({
+        where: {
+          postId: +postId,
+          password,
+        },
+      });
+  
+      return res.status(200).json({ message: '게시글을 삭제하였습니다.' });
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(400)
+        .json({ message: '데이터 형식이 올바르지 않습니다.' });
     }
-
-});
-
-
-
-
-
-export default router;
+  });
+  
+  export default router;
+  
