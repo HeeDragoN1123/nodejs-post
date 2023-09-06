7e10ffa86e7f58079d4d7e8b7e97aa302fc7c6de
HeeDragoN1123 <yhy7952@gmail.com>
2023-09-06 15:18:59 +0900

    초기 세팅

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import postRouter from './post.router.js';
 import commnetRouter from './comments.router.js';
-
+import signRouter from './sign.router.js';
 export default {
+  signRouter,
   postRouter,
   commnetRouter,
 };
