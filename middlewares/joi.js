7e834d5133ab713655aab9b53520f036ab3a6b69
HeeDragoN1123 <yhy7952@gmail.com>
2023-09-07 14:47:00 +0900

    joi 작성 게시글 에러 표시 시작

--- middlewares/joi.js
+++ middlewares/joi.js
@@ -2,13 +2,12 @@ import Joi from 'joi';
 
 // Joi 스키마를 정의합니다.
 const userSchema = Joi.object({
-    nickname: Joi.string()
-      .min(3)
-      .pattern(/^[a-zA-Z0-9]+$/)
-      .required(),
-    password: Joi.string().min(4).required(),
+    nickname: Joi.string().min(3).pattern(/^[a-zA-Z0-9]+$/).required(),
+    password: Joi.string().min(4).pattern(/^[a-zA-Z0-9]+$/).required(),
     confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
   });
 
+
+  
 
   
